f5ddd9d1738ee4f4e89081875fb6a9694cb88b03
DevWalissonMatheus <mwalisom9@gmail.com>
2022-07-01 19:43:22 -0300

    Começando o treinamento

--- Exercicio.py
+++ Exercicio.py
@@ -1,2 +1,15 @@
-# Texte para criar o repositório
-# Teste
+while True:
+    try:
+        prim_num = float(input('Insira o primeiro número: '))
+        seg_num = float(input('Insira o segundo número: '))
+        if prim_num > seg_num:
+            print('O maior número é: ', prim_num)
+            break
+        elif seg_num > prim_num:
+            print('O maior número é: ', seg_num)
+            break
+        else:
+            print('ERRO!\nTente novamente')
+            continue
+    except ValueError:
+        print('Insira um valor númerico')
