8cc278f1c65ed72c0befc1eee14eb68e699331e3
DevWalissonMatheus <mwalisom9@gmail.com>
2022-07-02 09:37:36 -0300

    Primeiro exercicio concluido

--- Exercicio.py
+++ Exercicio.py
@@ -1,15 +1,20 @@
+print("Qual o maior número?\n")
 while True:
     try:
         prim_num = float(input('Insira o primeiro número: '))
+        if prim_num >= 0:
+            break
+    except ValueError:
+        print('Insira um valor númerico')
+while True:
+    try:
         seg_num = float(input('Insira o segundo número: '))
-        if prim_num > seg_num:
-            print('O maior número é: ', prim_num)
-            break
-        elif seg_num > prim_num:
-            print('O maior número é: ', seg_num)
-            break
-        else:
-            print('ERRO!\nTente novamente')
-            continue
+        if seg_num >= 0:
+            if prim_num > seg_num:
+                print('O maior número é: ',prim_num)
+                break
+            elif seg_num > prim_num:
+                print('O maior número é: ',seg_num)
+                break
     except ValueError:
         print('Insira um valor númerico')
